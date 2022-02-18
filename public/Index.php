05f08a09be24b66462d04e1a81a18dd5a92ec83d
Sandra <sandra.villot038@gmail.com>
2022-02-18 16:01:59 +0100

    début css

--- public/Index.php
+++ public/Index.php
@@ -5,14 +5,56 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css\style.css">
-    <title>Portfolio
-    </title>
+    <title>Portfolio</title>
 </head>
 <body>
     <?Php
     ?>
-    <h1>Mon Portfolio</h1>
-<p>Je teste ma nouvelle branche</p>
-<p>Je reteste ma branche</p>
+    <section class="presentation">
+        <header>
+            <nav>
+                <li><a href="">Présentation</a></li>
+                <li><a href="">Compétences</a></li>
+                <li><a href="">Mes projets</a></li>
+                <li><a href="">Contact</a></li>
+            </nav>
+        </header>
+        <div>
+            <h1>Sandra Villot</h1>
+            <p>Développeuse Web & Mobile</p>
+        </div>
+    </section>
+    <section>
+        <h2>Mes compétences</h2>
+    </section>
+    <section>
+        <h2>Mes projets</h2>
+            <div class="projet-container">
+                <div class="projet-item">
+                    <div>image</div>
+                    <div>texte</div>
+                </div>
+            </div>
+    </section>
+    <footer>
+        <section>
+            <h2>Me conctacter</h2>
+            <form action="" method="post">
+                <label for="first-name"></label>
+                <input type="text" id="first-name" name="prenom" placeholder="Prénom">
+
+                <label for="last-name"></label>
+                <input type="text" id="last-name" name="nom" placeholder="Nom">
+
+                <label for="mail"></label>
+                <input type="email" name="email" id="mail" placeholder="Email">
+
+                <label for="message"></label>
+                <textarea name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+
+                <input type="submit" value="Envoyer">
+            </form>
+        </section>
+    </footer>
 </body>
 </html>
